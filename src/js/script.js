768379d4594ea83c878d9bbeae60d5cc6f436ad9
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2025-11-13 14:40:15 +0100

    add product render

--- src/js/script.js
+++ src/js/script.js
@@ -1,82 +1,111 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
-{
-  'use strict';
 
-  const select = {
-    templateOf: {
-      menuProduct: '#template-menu-product',
+'use strict';
+
+const select = {
+  templateOf: {
+    menuProduct: '#template-menu-product',
+  },
+  containerOf: {
+    menu: '#product-list',
+    cart: '#cart',
+  },
+  all: {
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+    formInputs: 'input, select',
+  },
+  menuProduct: {
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
+    cartButton: '[href="#add-to-cart"]',
+  },
+  widgets: {
+    amount: {
+      input: 'input[name="amount"]',
+      linkDecrease: 'a[href="#less"]',
+      linkIncrease: 'a[href="#more"]',
     },
-    containerOf: {
-      menu: '#product-list',
-      cart: '#cart',
-    },
-    all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
-    },
-    menuProduct: {
-      clickable: '.product__header',
-      form: '.product__order',
-      priceElem: '.product__total-price .price',
-      imageWrapper: '.product__images',
-      amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
-    },
-    widgets: {
-      amount: {
-        input: 'input[name="amount"]',
-        linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
-  };
+  },
+};
 
-  const classNames = {
-    menuProduct: {
-      wrapperActive: 'active',
-      imageVisible: 'active',
-    },
-  };
+const classNames = {
+  menuProduct: {
+    wrapperActive: 'active',
+    imageVisible: 'active',
+  },
+};
 
-  const settings = {
-    amountWidget: {
-      defaultValue: 1,
-      defaultMin: 0,
-      defaultMax: 10,
-    }
-  };
-
-  const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-  };
-
-  const app = {
-    initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
-    },
-
-    init: function(){
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-
-      thisApp.initMenu();
-    },
-  };
-
-  class Product{
-    constructor(){
-      const thisProduct = this;
-
-      console.log('new Product:', thisProduct);
-    }
+const settings = {
+  amountWidget: {
+    defaultValue: 1,
+    defaultMin: 0,
+    defaultMax: 10,
   }
+};
 
-  app.init();
+const templates = {
+  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+};
+
+const app = {
+
+  initMenu: function(){
+    const thisApp = this;
+    console.log('thisApp.data:', thisApp.data);
+    // Przekazanie nazwy właściwości oraz obiektu.
+    for(let productData in thisApp.data.products){
+      new Product(productData, thisApp.data.products[productData]);
+    }
+  },
+
+  init: function(){
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
+
+    thisApp.initData();
+    thisApp.initMenu();
+  },
+
+  initData: function(){
+    const thisApp = this;
+
+    thisApp.data = dataSource;
+  },
+};
+
+class Product{
+  constructor(id, data){
+    const thisProduct = this;
+    
+    thisProduct.id = id;
+    thisProduct.data = data;
+
+    thisProduct.renderInMenu();
+
+    console.log('new Product:', thisProduct);
+  }
+  
+
+  renderInMenu(){
+    const thisProduct = this;
+    // wygenerowanie HTML opartego o template
+    const generatedHTML = templates.menuProduct(thisProduct.data);
+    //console.log(generatedHTML);
+    // stworzenie elementu using utils.createElementFromHTML
+    thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+    // znalezienie container menu
+    const menuContainer = document.querySelector(select.containerOf.menu);
+    // dodanie elementu do menu
+    menuContainer.appendChild(thisProduct.element);
+  }
 }
+app.init();
